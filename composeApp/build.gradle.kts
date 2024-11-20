22733166ed9eecaabbd19d059507c6a2b274d74c
Daniel Ramzani <danielramzani@gmail.com>
2024-11-20 20:46:25 +0000

    updated to java 17

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
     }
@@ -156,8 +156,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
