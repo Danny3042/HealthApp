bfdccc300981ea23035002bb4616d5e2c9cfa65d
Danny3042 <114992445+DanielR2004@users.noreply.github.com>
2025-10-07 13:39:07 +0100

    updated files

--- composeApp/src/commonMain/kotlin/utils/HabitRepository.kt
+++ composeApp/src/commonMain/kotlin/utils/HabitRepository.kt
@@ -3,11 +3,23 @@ package utils
 
 object HabitRepository {
     var completedHabits = HabitStorage.loadHabits().toMutableList()
+    val habits: List<String> get() = completedHabits
 
     fun addCompletedHabit(habit: String) {
         if (!completedHabits.contains(habit)) {
             completedHabits.add(habit)
             HabitStorage.saveHabits(completedHabits)
         }
+    }
+
+    fun addHabit(habit: String) {
+        if (habit.isNotBlank() && !habits.contains(habit)) {
+            completedHabits.add(habit)
+            HabitStorage.saveHabits(completedHabits)
+        }
+    }
+    fun removeHabit(habit: String) {
+        completedHabits.remove(habit)
+        HabitStorage.saveHabits(completedHabits)
     }
 }
