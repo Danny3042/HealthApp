e2348aa955539d3b6a4f0a3a45f344590692738d
Danny3042 <d.ramzani-2021@hull.ac.uk>
2025-10-20 01:04:00 +0100

    Added new app icons for iOS 26

--- composeApp/src/commonMain/kotlin/tabs/InsightsTab.kt
+++ composeApp/src/commonMain/kotlin/tabs/InsightsTab.kt
@@ -1,6 +1,5 @@
 package tabs
 
-import InsightsPage
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Insights
 import androidx.compose.runtime.Composable
@@ -9,6 +8,7 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.navigation.compose.rememberNavController
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import pages.InsightsPage
 
 object InsightsTab : Tab {
 
